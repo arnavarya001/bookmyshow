af3883092837172b2a148d2182aa82b14647d0bb
Arnav Arya <arnavarya2004@gmail.com>
2026-09-02 16:03:24 +0530

    fix(vercel): use modern zero-config rewrites in vercel.json

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
-module.exports = (req, res) => {
-  try {
-    const app = require("../app");
-    return app(req, res);
-  } catch (err) {
-    console.error("Vercel Startup Error:", err);
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "text/plain");
-    return res.end(`Vercel Startup Error:\n${err.message}\n\nStack:\n${err.stack}`);
-  }
-};
+const app = require("../app");
+
+module.exports = app;

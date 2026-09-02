b065cd5d69447d0ec5eb267b3271bae3e392ad75
Arnav Arya <arnavarya2004@gmail.com>
2026-09-02 23:49:45 +0530

    fix(vercel): export Express app instance directly for Vercel bridge

--- api/index.js
+++ api/index.js
@@ -1,10 +1,3 @@
-module.exports = (req, res) => {
-  try {
-    const app = require("../app");
-    return app(req, res);
-  } catch (err) {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "text/plain");
-    return res.end(`Vercel Startup Error:\n${err.message}\n\nStack:\n${err.stack}`);
-  }
-};
+const app = require("../app");
+
+module.exports = app;

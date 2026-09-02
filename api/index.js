5a0826bdd3fe49ac1434a3174b420424a086f62f
Arnav Arya <arnavarya2004@gmail.com>
2026-09-02 16:02:17 +0530

    fix(vercel): add diagnostic handler in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-const app = require("../app");
-
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    const app = require("../app");
+    return app(req, res);
+  } catch (err) {
+    console.error("Vercel Startup Error:", err);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    return res.end(`Vercel Startup Error:\n${err.message}\n\nStack:\n${err.stack}`);
+  }
+};

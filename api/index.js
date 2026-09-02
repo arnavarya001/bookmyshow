5a707cf17eb43c53ec26c33cd5040cbb729d1962
Arnav Arya <arnavarya2004@gmail.com>
2026-09-02 16:04:57 +0530

    fix(vercel): catch and return exact startup error

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-const app = require("../app");
-
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    const app = require("../app");
+    return app(req, res);
+  } catch (err) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    return res.end(`Vercel Startup Error:\n${err.message}\n\nStack:\n${err.stack}`);
+  }
+};
